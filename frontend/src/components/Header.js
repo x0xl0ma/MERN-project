b0c15ec7fd45660c9c3d8508e0ca60f8645bfc8b
xoxloma <k0tjaka@yandex.ru>
2021-01-03 16:47:14 +0300

    add header and footer

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/card">
+                <i className="fas fa-shopping-cart" /> Cart
+              </Nav.Link>
+              <Nav.Link href="/loggin">
+                <i className="fas fa-user" /> Log in
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
